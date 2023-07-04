2fc756acf312b43a450acfccf3414b7bb05e61f7
oplevan <oleg.plevan@gmail.com>
2023-07-04 16:00:13 +0100

    custom scrollbar

---
+++ app/components/Navigation/Navigation.tsx
@@ -0,0 +1,8 @@
+export default function Navigation() {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi eius placeat id exercitationem fugit temporibus accusamus molestiae consequuntur, eveniet deleniti
+            architecto quo veniam atque minus quae, dolore aspernatur accusantium reiciendis!
+        </div>
+    );
+}
